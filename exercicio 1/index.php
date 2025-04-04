54356a31128e7d716ad119968a71e5b33eb61209
dudex-computer <cauaalborghette09@gmail.com>
2025-04-04 08:13:13 -0300

    fazendo atividade 1

---
+++ exercicio 1/index.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mensagem PHP</title>
+</head>
+<body>
+    <h1>Mensagem</h1>
+    <div>
+        <?php 'mensagem.php'; ?>
+    </div>
+</body>
+</html>
